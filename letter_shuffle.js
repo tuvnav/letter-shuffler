52d94c745c855c5a6e2de8788be176c12fcce383
tuvnav <105378329+tuvnav@users.noreply.github.com>
2023-02-22 13:52:18 +0100

    added difficulty levels and basic styles

--- letter_shuffle.js
+++ letter_shuffle.js
@@ -1,35 +1,61 @@
-function shuffle(line) {
+function shuffle(line, difficulty) {
     let shuffledLine
     if (line.includes(' ')) {
         let shuffledLineArr = []
         const phrase = line.split(' ')
         for (let word of phrase) { 
-            shuffledLineArr.push(shuffleWord(word.toUpperCase()))
+            shuffledLineArr.push(shuffleWord(word.toUpperCase(), difficulty))
         }
         shuffledLine = shuffledLineArr.join(' ')
     }
     else {
         const word = line
-        shuffledLine = shuffleWord(word.toUpperCase())
+        shuffledLine = shuffleWord(word.toUpperCase(), difficulty)
     }
     return shuffledLine
 }
 
-function shuffleWord(word) {
+function shuffleWord(word, difficulty) {
     const l = word.length
     let shuffledWord 
-    if (l <= 5) {
+    let wordChunkLength // the number of characters that are allowed to reappeare in the same order as in the original word
+    if (difficulty === 'easy') {
+        wordChunkLength = l >= 3 ? l : 2 
+        if (l <= 3) {
+            shuffledWord = shuffleLetters(word)
+        }
+        else if (l <= 5) {
+            shuffledWord = shuffleMiddleLetters(word) 
+        }
+        else if (l > 5) {
+            const firstLetter = word[0]
+            const lastLetter = word[(l - 1)]
+            const middleLetters = word.slice(1, (l - 1))
+            const shuffledMiddleLetters = shuffleLetterChunks(middleLetters, 3)
+            shuffledWord = firstLetter + shuffledMiddleLetters + lastLetter
+        }
+    }
+    else if (difficulty === 'medium') {
+        wordChunkLength = l >= 3 ? 3 : 2
+        if (l <= 5) {
+            shuffledWord = shuffleLetters(word)
+        }
+        else if (l <= 8) {
+            shuffledWord = shuffleLetterChunks(word, 2)
+        }
+        else if (l > 8) {
+            shuffledWord = shuffleMiddleLetters(word)
+        }
+    }
+    else if (difficulty === 'hard') {
+        wordChunkLength = l >= 3 ? 3 : 2
         shuffledWord = shuffleLetters(word)
     }
-    else if (l <= 8) {
-        shuffledWord = shuffleLetterPairs(word)
+    if (checkForWordChunks(word, shuffledWord, wordChunkLength) == "word Chunk found") {
+        shuffledWord = shuffleWord(word, difficulty)
     }
-    else if (l > 8) {
-        shuffledWord = shuffleMiddleLetters(word)
-    }
-    if (checkForWordChunks(word, shuffledWord) == "word Chunk found") {
-        shuffledWord = shuffleWord(word)
-    }
+    
+
     return shuffledWord
 }
 
@@ -42,21 +68,20 @@ function shuffleLetters(word) {
     return shuffledLetters;                        
 }
 
-function shuffleLetterPairs(word) {
-    let letterList = []
-    let i = 0
-    let letters
-    while (i < word.length) {
-        let randomPosition = Math.floor(Math.random() * (letterList.length + 1))
-        if (i == (word.length - 1)) {
-            letters = word[i]
-        } else {
-            letters = word[i] + word[i+1] 
+function shuffleLetterChunks(word, chunkLength) {
+    let wordList = word.split('')
+    wordList.forEach((item, i, array) => { // insert a whitespace before every letter group of the length "chunkLength" 
+        if (i % chunkLength === 0 && i !== 0) {
+            array[i] = ' ' + item
         }
-        letterList.splice(randomPosition, 0, letters)
-        i += 2
-    }
-    const shuffledWord = letterList.join('') 
+    })
+    const newWord = wordList.join('')
+    wordList = newWord.split(' ')
+    wordList.sort(function() { // shuffle order of the letter groups list
+        return 0.5 - Math.random();
+    })
+    const shuffledWord = wordList.join('')
+
     return shuffledWord
 }
 
@@ -68,19 +93,19 @@ function shuffleMiddleLetters(word) {
     return firstLetter + shuffledMiddleLetters + lastLetter
 }
 
-function checkForWordChunks(word, shuffledWord) {
+function checkForWordChunks(word, shuffledWord, wordChunkLength) {
     let wordChunksArr = []
     let shuffledWordChunksArr = []
-    for (let i = 0; i < (word.length - 2); i++) {
-        const wordChunk = word.slice(i, i + 3)
+    for (let i = 0; i < (word.length - (wordChunkLength - 1)); i++) {
+        const wordChunk = word.slice(i, i + wordChunkLength)
         wordChunksArr.push(wordChunk)
     }
-    for (let j = 0; j < (shuffledWord.length - 2); j++) {
-        const shuffledWordChunk = shuffledWord.slice(j, j + 3)
+    for (let j = 0; j < (shuffledWord.length - (wordChunkLength - 1)); j++) {
+        const shuffledWordChunk = shuffledWord.slice(j, j + wordChunkLength)
         shuffledWordChunksArr.push(shuffledWordChunk)
     }
-    for (let i = 0; i < (word.length - 2); i++) {
-        for (let j = 0; j < (shuffledWord.length - 2); j++) {
+    for (let i = 0; i < wordChunksArr.length; i++) {
+        for (let j = 0; j < shuffledWordChunksArr.length; j++) {
             if (wordChunksArr[i] === shuffledWordChunksArr[j]) {
                 return "word Chunk found"
             }
@@ -89,25 +114,34 @@ function checkForWordChunks(word, shuffledWord) {
 }
 
 
-const fileInput = document.getElementById('fileinput')
+const form = document.querySelector("form")
+const fileInput = document.getElementById("fileinput")
+const inputTextArea = document.getElementById("textinput")
 const output = document.getElementById("output")
 
-let defaultString = "RHEE ERA ESMO \nEMAPLEEX RDWOS"
-output.textContent=defaultString
+const defaultString = "Your shuffled words will be displayed here"
+output.textContent = defaultString
 
-fileInput.addEventListener('change', () => {
+fileInput.addEventListener("change", () => {
     const fr = new FileReader()
     fr.readAsText(fileInput.files[0])
     fr.onload = () => {
-        content = fr.result
-        
-        let newContent = []
-        const lines = content.split(/\r?\n/)
-        for (const line of lines) {
-            newContent.push(shuffle(line))
-        }
-
-        output.textContent=newContent.join('\n')
+        const fileContent = fr.result
+        inputTextArea.value = fileContent
     }
+}
+)
+
+form.addEventListener("submit", (event) => {
+    const formData = new FormData(form)
+    const textInput = formData.get("textinput")
+    const difficulty = formData.get("difficulty")
+    let newContent = []
+    const lines = textInput.split(/\r?\n/)
+    for (const line of lines) {
+        newContent.push(shuffle(line, difficulty))
+    }
+    output.textContent = newContent.join('\n')
+    event.preventDefault()
 }
 )
